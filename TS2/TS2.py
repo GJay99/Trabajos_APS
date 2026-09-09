1070d940ccc9da101e162b574d06efa632e4e3fe
GJay99 <gonzalojay05@gmail.com>
2026-09-09 16:42:49 -0300

    Agrego TS2.ipynb

--- TS2/TS2.py
+++ TS2/TS2.py
@@ -40,13 +40,14 @@ def mi_fft(x,ref=0):
 t,s = mi_funcion_seno(vmax = np.sqrt(2),ff=fs/N)
 
 n_bits = 4
-k_n = 1
+k_n = 10
 Vfs = 2
 q = (2*Vfs)/(np.pow(2,n_bits))
 P_q = q**2/12
 P_n = k_n * P_q
 sigma = np.sqrt(P_n)
 n = np.random.normal(scale=sigma,size=len(t))
+print(np.mean(n))
 
 sn = s+n
 
@@ -66,20 +67,22 @@ plt.axvline(0,color="black",linewidth=1)
 plt.legend()
 
 freq, sn_fft_abs = mi_fft(sn)
-ref = np.max(sn_fft_abs)
-
-sn_fft_dB = 20 * np.log10(sn_fft_abs/ref)
+sn_fft_power = 2 * (sn_fft_abs/N)**2
+sn_fft_dB = 10 * np.log10(sn_fft_power)
 
 freq, sn_cuantizada_fft_abs = mi_fft(sn_cuantizada)
-sn_cuantizada_fft_dB = 20 * np.log10(sn_cuantizada_fft_abs/ref)
+sn_cuantizada_fft_power = 2 * (sn_cuantizada_fft_abs/N)**2
+sn_cuantizada_fft_dB = 10 * np.log10(sn_cuantizada_fft_power)
 
 freq, n_fft_abs = mi_fft(n)
-n_fft_dB = 20 * np.log10(n_fft_abs/ref)
+n_fft_power = 2 * (n_fft_abs/N)**2
+n_fft_dB = 10 * np.log10(n_fft_power)
 
 n_q = sn_cuantizada - sn
-freq, n_q_fft_abs = mi_fft(n_q)
-n_q_fft_dB = 20 * np.log10(n_q_fft_abs/ref)
 
+freq, n_q_fft_abs = mi_fft(n_q)
+n_q_fft_power = 2 * (n_q_fft_abs/N)**2
+n_q_fft_dB = 10 * np.log10(n_q_fft_power)
 plt.figure(2)
 plt.plot(freq,sn_cuantizada_fft_dB,color="blue",label="$Sn_q$ = $Q_{B,V_f}$ (ADC out)")
 plt.plot(freq,n_fft_dB,linestyle ="--", color="red")
