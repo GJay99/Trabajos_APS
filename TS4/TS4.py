7335678e3758df36b46e62f645a3c8a457083653
GJay99 <gonzalojay05@gmail.com>
2026-09-20 00:50:17 -0300

    Agrego TS4

--- TS4/TS4.py
+++ TS4/TS4.py
@@ -30,6 +30,7 @@ nn = np.arange(0,N,1)
 s = np.sqrt(2)*np.sin(np.outer(omega_uno,nn))
 Ps = np.var(s)
 SNR = 3
+print(f"Caso SNR = {SNR}")
 sigma = np.sqrt(Ps/np.power(10,SNR/10)) 
 
 n_a = np.random.normal(scale=sigma,size=s.shape)
@@ -47,24 +48,15 @@ a_1_rect = x_fft_abs[:,freq==(omega_cero/(2*np.pi))*N]
 
 
 indice_maximo = np.argmax(x_fft_abs, axis=1)
-omega_rect = freq[indice_maximo]*2*np.pi/N
-print("Media estimador de amp rectang:", np.mean(a_1_rect))
-print("Media estimador de fre rectang:",np.mean(omega_rect))
-"""
-espa_1_rect = np.mean(a_1_rect)
-sa_1_rect=espa_1_rect-np.sqrt(2)
-print(sa_1_rect)
-espa_1_rect = np.var(a_1_rect)
-sa_1_rect=espa_1_rect-np.sqrt(2)
-print(sa_1_rect)
-#pot = np.mean(x**2)
-#print("Potencia blackmanh:", pot)
+omega_rect = freq[indice_maximo]*2*np.pi/fs
+print("\nVentana rectangular:")
+print(f"Media est_a1:{np.mean(a_1_rect)} y Varianza:{np.var(a_1_rect)}")
+sesgo_a1 = np.mean(a_1_rect) - np.sqrt(2)
+print(f"Sesgo:{sesgo_a1}")
+print(f"Media est_omega:{np.mean(omega_rect)} y Varianza:{np.var(omega_rect)}")
+sesgo_omega = np.mean(omega_rect-omega_uno)
+print(f"Sesgo:{sesgo_omega}")
 
-#par = np.sum(x_fft_abs**2, axis=1) / (2)
-#par = np.mean(par)
-
-#print("Potencia Parseval:", par)
-"""
 #%% Flattop 3dB
 
 flat=win.flattop(N)
@@ -80,9 +72,14 @@ a_1_flat = x_fft_abs[:,freq==(omega_cero/(2*np.pi))*N]
 
 
 indice_maximo = np.argmax(x_fft_abs, axis=1)
-omega_flat = freq[indice_maximo]*2*np.pi/N
-print("Media estimador de amp flattop:",np.mean(a_1_flat))
-print("Media estimador de fre flattop:",np.mean(omega_flat))
+omega_flat = freq[indice_maximo]*2*np.pi/fs
+print("\nVentana flat-top:")
+print(f"Media est_a1:{np.mean(a_1_flat)} y Varianza:{np.var(a_1_flat)}")
+sesgo_a1 = np.mean(a_1_flat) - np.sqrt(2)
+print(f"Sesgo:{sesgo_a1}")
+print(f"Media est_omega:{np.mean(omega_flat)} y Varianza:{np.var(omega_flat)}")
+sesgo_omega = np.mean(omega_flat-omega_uno)
+print(f"Sesgo:{sesgo_omega}")
 
 #%% Blackmanharris 3dB
 
@@ -98,9 +95,31 @@ plt.xlim(240,260)
 a_1_blackh = x_fft_abs_norm[:,freq==(omega_cero/(2*np.pi))*N]
 
 indice_maximo = np.argmax(x_fft_abs_norm, axis=1)
-omega_blackh = freq[indice_maximo]*2*np.pi/N
-print("Media estimador de amp blackmanh:",np.mean(a_1_blackh))
-print("Media estimador de fre blackmanh:",np.mean(omega_blackh))
+omega_blackh = freq[indice_maximo]*2*np.pi/fs
+print("\nVentana Blackman-Harris:")
+print(f"Media est_a1:{np.mean(a_1_blackh)} y Varianza:{np.var(a_1_blackh)}")
+print(f"Media est_omega:{np.mean(omega_blackh)} y Varianza:{np.var(omega_blackh)}")
+sesgo_omega = np.mean(omega_flat-omega_uno)
+print(f"Sesgo:{sesgo_omega}")
+
+#%% Hamming 3dB
+
+hamm = win.hamming(N)
+x_hamm = x * hamm
+
+freq, x_fft_abs = mi_fft(x_hamm)
+x_fft_abs_norm = 2*x_fft_abs/N
+x_fft_abs_traspuesta = np.transpose(x_fft_abs_norm)
+plt.figure(4)
+plt.plot(freq,x_fft_abs_traspuesta,":.")
+plt.xlim(240,260)
+a_1_hamm = x_fft_abs_norm[:,freq==(omega_cero/(2*np.pi))*N]
+
+indice_maximo = np.argmax(x_fft_abs_norm, axis=1)
+omega_hamm = freq[indice_maximo]*2*np.pi/fs
+print("\nVentana Hamming:")
+print(f"Media est_a1:{np.mean(a_1_hamm)} y Varianza:{np.var(a_1_hamm)}")
+print(f"Media est_omega:{np.mean(omega_hamm)} y Varianza:{np.var(omega_hamm)}")
 
 #%% Histogramas 3dB
 
@@ -108,47 +127,19 @@ plt.figure(5)
 plt.hist(a_1_rect, bins=20, color="red")
 plt.hist(a_1_flat, bins=20, color="blue")
 plt.hist(a_1_blackh, bins=20, color="gold", alpha=0.7)
+plt.hist(a_1_hamm, bins=20, color="green", alpha=0.7)
+
 
 plt.figure(6)
 plt.hist(omega_rect, bins=20, color="red", histtype="step", linewidth=2)
 plt.hist(omega_flat, bins=20, color="blue", histtype="step", linewidth=2)
 plt.hist(omega_blackh, bins=20, color="gold", histtype="step", linewidth=2)
-
-"""
-plt.figure(5)
-
-plt.hist(a_1_rect, bins=20, alpha=0.5, color="red", label="Rectangular")
-plt.hist(a_1_flat, bins=20, alpha=0.5, color="blue", label="Flattop")
-plt.hist(a_1_blackh, bins=20, alpha=0.5, color="gold", label="Blackman-Harris")
-
-plt.xlabel("Amplitud")
-plt.ylabel("Cantidad de muestras")
-plt.title("Histograma de amplitudes - SNR = 3 dB")
-plt.legend()
-plt.grid(alpha=0.2)
-
-
-plt.figure(6)
-
-plt.hist(omega_rect, bins=20, alpha=0.5, color="red", label="Rectangular")
-plt.hist(omega_flat, bins=20, alpha=0.5, color="blue", label="Flattop")
-plt.hist(omega_blackh, bins=20, alpha=0.5, color="gold", label="Blackman-Harris")
-
-plt.xlabel("Frecuencia")
-plt.ylabel("Cantidad de muestras")
-plt.title("Histograma de frecuencias - SNR = 3 dB")
-plt.legend()
-plt.grid(alpha=0.2)
-
-plt.show()
-"""
-
-
-
+plt.hist(omega_hamm, bins=20, color="green", histtype="step", linewidth=2)
 
 #%% Para SNR=10
 
-SNR = 3
+SNR = 10
+print(f"\n\nCaso SNR = {SNR}")
 sigma = np.sqrt(Ps/np.power(10,SNR/10)) 
 
 n_a = np.random.normal(scale=sigma,size=s.shape)
@@ -165,9 +156,10 @@ plt.xlim(240,260)
 a_1_rect = x_fft_abs[:,freq==(omega_cero/(2*np.pi))*N]
 
 indice_maximo = np.argmax(x_fft_abs, axis=1)
-omega_rect = freq[indice_maximo]*2*np.pi/N
-print("Media estimador de amp rectang:", np.mean(a_1_rect))
-print("Media estimador de fre rectang:",np.mean(omega_rect))
+omega_rect = freq[indice_maximo]*2*np.pi/fs
+print("\nVentana rectangular:")
+print(f"Media est_a1:{np.mean(a_1_rect)} y Varianza:{np.var(a_1_rect)}")
+print(f"Media est_omega:{np.mean(omega_rect)} y Varianza:{np.var(omega_rect)}")
 
 #%% Flattop 10dB
 
@@ -184,9 +176,11 @@ a_1_flat = x_fft_abs[:,freq==(omega_cero/(2*np.pi))*N]
 
 
 indice_maximo = np.argmax(x_fft_abs, axis=1)
-omega_flat = freq[indice_maximo]*2*np.pi/N
-print("Media estimador de amp flattop:",np.mean(a_1_flat))
-print("Media estimador de fre flattop:",np.mean(omega_flat))
+omega_flat = freq[indice_maximo]*2*np.pi/fs
+
+print("\nVentana Flat-Top:")
+print(f"Media est_a1:{np.mean(a_1_flat)} y Varianza:{np.var(a_1_flat)}")
+print(f"Media est_omega:{np.mean(omega_flat)} y Varianza:{np.var(omega_flat)}")
 
 #%% Blackmanharris 10dB
 
@@ -202,9 +196,30 @@ plt.xlim(240,260)
 a_1_blackh = x_fft_abs_norm[:,freq==(omega_cero/(2*np.pi))*N]
 
 indice_maximo = np.argmax(x_fft_abs_norm, axis=1)
-omega_blackh = freq[indice_maximo]*2*np.pi/N
-print("Media estimador de amp blackmanh:",np.mean(a_1_blackh))
-print("Media estimador de fre blackmanh:",np.mean(omega_blackh))
+omega_blackh = freq[indice_maximo]*2*np.pi/fs
+
+print("\nVentana Blackman-Harris:")
+print(f"Media est_a1:{np.mean(a_1_blackh)} y Varianza:{np.var(a_1_blackh)}")
+print(f"Media est_omega:{np.mean(omega_blackh)} y Varianza:{np.var(omega_blackh)}")
+
+#%% Hamming 10dB
+
+hamm = win.hamming(N)
+x_hamm = x * hamm
+
+freq, x_fft_abs = mi_fft(x_hamm)
+x_fft_abs_norm = 2*x_fft_abs/N
+x_fft_abs_traspuesta = np.transpose(x_fft_abs_norm)
+plt.figure(10)
+plt.plot(freq,x_fft_abs_traspuesta,":.")
+plt.xlim(240,260)
+a_1_hamm = x_fft_abs_norm[:,freq==(omega_cero/(2*np.pi))*N]
+
+indice_maximo = np.argmax(x_fft_abs_norm, axis=1)
+omega_hamm = freq[indice_maximo]*2*np.pi/fs
+print("\nVentana Hamming:")
+print(f"Media est_a1:{np.mean(a_1_hamm)} y Varianza:{np.var(a_1_hamm)}")
+print(f"Media est_omega:{np.mean(omega_hamm)} y Varianza:{np.var(omega_hamm)}")
 
 #%% Histogramas 10dB
 
@@ -212,8 +227,10 @@ plt.figure(11)
 plt.hist(a_1_rect, bins=20, color="red")
 plt.hist(a_1_flat, bins=20, color="blue")
 plt.hist(a_1_blackh, bins=20, color="gold", alpha=0.7)
+plt.hist(a_1_hamm, bins=20, color="green", alpha=0.7)
 
 plt.figure(12)
 plt.hist(omega_rect, bins=20, color="red", histtype="step", linewidth=2)
 plt.hist(omega_flat, bins=20, color="blue", histtype="step", linewidth=2)
 plt.hist(omega_blackh, bins=20, color="gold", histtype="step", linewidth=2)
+plt.hist(omega_hamm, bins=20, color="green", histtype="step", linewidth=2)
